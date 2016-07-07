fdf0373a90c78857b8ea51e354a15d9caf5c1197
willriggins <williamriggins2013@gmail.com>
2016-07-07 17:24:30 -0400

    start rest controller

--- src/main/java/com/theironyard/controllers/UpdawwgRestController.java
+++ src/main/java/com/theironyard/controllers/UpdawwgRestController.java
@@ -1,11 +1,41 @@
 package com.theironyard.controllers;
 
+import com.theironyard.entities.User;
+import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.sql.SQLException;
 
 /**
  * Created by will on 7/7/16.
  */
 @RestController
 public class UpdawwgRestController {
+
+//    @Autowired
+//
+//    @Autowired
+
+    @PostConstruct
+    public void init() throws SQLException {
+        Server.createWebServer().start();
+    }
+
+//    @RequestMapping(path = "/users", method = RequestMethod.GET)
+//    public Iterable<User> getUsers() {
+//
+//    }
+
+
+    //@RequestMapping(path = "", method = RequestMethod.POST)
+
+    //@RequestMapping(path = "", method = RequestMethod.GET)
+    //@RequestMapping(path = "", method = RequestMethod.POST)
+
+
 
 }
