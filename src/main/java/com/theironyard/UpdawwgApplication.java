4737efa87e5e198aaaafbd1c12f2d7b8ba830637
willriggins <williamriggins2013@gmail.com>
2016-07-07 16:33:27 -0400

    initial import

---
+++ src/main/java/com/theironyard/UpdawwgApplication.java
@@ -0,0 +1,12 @@
+package com.theironyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdawwgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdawwgApplication.class, args);
+	}
+}
---
+++ src/main/java/com/theironyard/controllers/UpdawwgRestController.java
@@ -0,0 +1,11 @@
+package com.theironyard.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by will on 7/7/16.
+ */
+@RestController
+public class UpdawwgRestController {
+
+}
